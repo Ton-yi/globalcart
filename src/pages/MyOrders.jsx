bc58714c678cc7303b0fea75e33e6c934c38ec87
同一（tongyi） <itsuchabeautifulday@gmail.com>
2026-03-19 17:42:48 +0000

    File changes

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -180,6 +180,7 @@ export default function MyOrders() {
                   {col.label}
                 </th>
               ))}
+              <th className="px-3 py-2"></th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-100">
