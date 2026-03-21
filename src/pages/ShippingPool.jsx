51c23d7fc58e61f3a082de7fa680cab5252ec55e
同一（tongyi） <itsuchabeautifulday@gmail.com>
2026-03-21 08:27:26 +0000

    File changes

--- src/pages/ShippingPool.jsx
+++ src/pages/ShippingPool.jsx
@@ -114,6 +114,9 @@ export default function ShippingPool() {
     setSelectedOrderIds([]);
     setForm({ recipient_name: "", recipient_phone: "", address_line1: "", address_line2: "", city: "", state: "", postal_code: "", destination_country: "", shipping_method: "", scheduled_ship_date: "", transit_location_id: "", user_note: "" });
     setConsType("");
+    setIsPrivate(false);
+    setSharedWithEmails([]);
+    setUserSearchQuery("");
     setFormLoading(true);
     const [orders, allLocs, prefs, usersRes] = await Promise.all([
       base44.entities.Order.filter({ user_email: user.email, order_status: "in_warehouse" }, "-updated_date", 100),
