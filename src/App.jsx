9b717afd65f251bfb5043811ec3add7cfe215abf
同一（tongyi） <itsuchabeautifulday@gmail.com>
2026-04-02 17:49:42 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const AuthenticatedApp = () => {
           <MainPage />
         </LayoutWrapper>
       } />
-      {Object.entries(Pages).map(([path, Page]) => (
+      {Object.entries(Pages).filter(([path]) => path !== 'ShippingPool').map(([path, Page]) => (
         <Route
           key={path}
           path={`/${path}`}
